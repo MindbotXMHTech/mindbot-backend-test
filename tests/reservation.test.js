7a4b5b83ea80e12fd6207fa0c9d3eb9a9d4e2cc6
Kim Hanz <nanaka3@hotmail.com>
2025-09-30 14:40:03 +0700

    test: add integration tests

--- tests/reservation.test.js
+++ tests/reservation.test.js
@@ -11,3 +11,30 @@ test("healthcheck works", async () => {
 });
 
 // TODO: Candidate must add tests for reservation flow
+test("can create a reservation", async () => {
+  const res = await fetch(`${API}/reservations`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      room_id: 101,
+      check_in: "2025-10-01",
+      check_out: "2025-10-05",
+    }),
+  });
+  assert.strictEqual(res.status, 201);
+  const body = await res.json();
+  assert.strictEqual(body.room_id, 101);
+});
+
+test("prevents double booking", async () => {
+  const res = await fetch(`${API}/reservations`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      room_id: 101,
+      check_in: "2025-10-01",
+      check_out: "2025-10-05",
+    }),
+  });
+  assert.strictEqual(res.status, 409);
+});
